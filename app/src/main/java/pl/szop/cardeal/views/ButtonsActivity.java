9bbe4e9dfdd269bd8fd2081887a3e76f2889769f
Unknown <oktawianlasek@gmail.com>
2018-11-25 11:37:36 +0100

    przywrocone R

---
+++ app/src/main/java/pl/szop/cardeal/views/ButtonsActivity.java
@@ -0,0 +1,15 @@
+package pl.szop.cardeal.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pl.szop.cardeal.R;
+
+public class ButtonsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_buttons);
+    }
+}
