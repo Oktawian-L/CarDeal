2a5149494092f086ff91291b4c08b0e1584d603c
Unknown <oktawianlasek@gmail.com>
2018-12-02 13:57:29 +0100

    somethinf for the ages

--- app/src/main/java/pl/szop/andrzejshop/views/CategoryActivity.java
+++ app/src/main/java/pl/szop/andrzejshop/views/CategoryActivity.java
@@ -54,51 +54,6 @@ public class CategoryActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ProductsActivity.class);
         intent.putExtra("categoryType", cat);
         startActivity(intent);
-    }/*
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_category);
-        Button btnCriminal = findViewById(R.id.button_criminal);
-
-        btnCriminal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startProductsListActivity("category");
-            }
-        });
-
-        Button btnFantasy = findViewById(R.id.button_fantasy);
-
-        btnFantasy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startProductsListActivity("fantastyka");
-            }
-        });
-
-        Button btnThriller = findViewById(R.id.button_thriller);
-
-        btnThriller.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startProductsListActivity("thriller");
-            }
-        });
-
-        Button btnSciFi = findViewById(R.id.button_scifi);
-
-        btnSciFi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startProductsListActivity("Science fiction");
-            }
-        });
-
     }
-    private void startProductsListActivity(String cat) {
-        Intent intent = new Intent(this, ProductsActivity.class);
-        intent.putExtra("categoryType", cat);
-        startActivity(intent);
-    }*/
+ 
 }
