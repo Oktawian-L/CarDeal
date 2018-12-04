a26cdce2d80806f3352d1e366aef392744fa91c6
Unknown <oktawianlasek@gmail.com>
2018-12-04 20:55:22 +0100

    lib z neta

--- app/src/main/java/pl/szop/andrzejshop/adapters/ImageAdapterSlide.java
+++ app/src/main/java/pl/szop/andrzejshop/adapters/ImageAdapterSlide.java
@@ -6,7 +6,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.content.Context;
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import java.util.ArrayList;
 import java.util.ArrayList;
 import android.content.Context;
 import android.support.annotation.NonNull;
@@ -19,44 +27,59 @@ import pl.szop.andrzejshop.R;
 
 public class ImageAdapterSlide extends PagerAdapter {
 
+        private ArrayList<Integer> IMAGES;
+        private LayoutInflater inflater;
+        private Context context;
 
-    private ArrayList<Integer> images;
-    private LayoutInflater inflater;
-    private Context context;
 
-    public ImageAdapterSlide(Context context, ArrayList<Integer> images) {
-        this.context = context;
-        this.images=images;
-        inflater = LayoutInflater.from(context);
+        public ImageAdapterSlide(Context context,ArrayList<Integer> IMAGES) {
+            this.context = context;
+            this.IMAGES=IMAGES;
+            inflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+        @Override
+        public int getCount() {
+            return IMAGES.size();
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup view, int position) {
+            View imageLayout = inflater.inflate(R.layout.view_pager_item, view, false);
+
+            assert imageLayout != null;
+            final ImageView imageView = (ImageView) imageLayout
+                    .findViewById(R.id.image);
+
+
+            imageView.setImageResource(IMAGES.get(position));
+
+            view.addView(imageLayout, 0);
+
+            return imageLayout;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view.equals(object);
+        }
+
+        @Override
+        public void restoreState(Parcelable state, ClassLoader loader) {
+        }
+
+        @Override
+        public Parcelable saveState() {
+            return null;
+        }
+
+
     }
-
-    @Override
-    public void destroyItem(ViewGroup container, int position, Object object) {
-        container.removeView((View) object);
-    }
-
-    @Override
-    public int getCount() {
-        return images.size();
-    }
-
-    @Override
-    public Object instantiateItem(ViewGroup view, int position) {
-        View myImageLayout = inflater.inflate(R.layout.view_pager_item, view, false);
-        ImageView myImage = (ImageView) myImageLayout
-                .findViewById(R.id.image);
-        myImage.setImageResource(images.get(position));
-        view.addView(myImageLayout, 0);
-        return myImageLayout;
-    }
-
-    @Override
-    public boolean isViewFromObject(View view, Object object) {
-        return view.equals(object);
-    }
-}
-
-
 
 
 
