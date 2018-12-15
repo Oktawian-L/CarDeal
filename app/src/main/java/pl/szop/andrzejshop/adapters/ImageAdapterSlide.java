a064563c5456582f2bce537de4fe55d495d877e3
Unknown <oktawianlasek@gmail.com>
2018-12-15 20:07:54 +0100

    necessary files

--- app/src/main/java/pl/szop/andrzejshop/adapters/ImageAdapterSlide.java
+++ app/src/main/java/pl/szop/andrzejshop/adapters/ImageAdapterSlide.java
@@ -41,7 +41,7 @@ public class ImageAdapterSlide extends PagerAdapter {
 
     private int[] sliderImagesId = new int[]{
             R.drawable.skoda, R.drawable.skoda_fabia_49, R.drawable.mustang2,
-            R.drawable.fabia_red, R.drawable.bmwindek, R.drawable.images,
+            R.drawable.fabia_red, R.drawable.fabia_red, R.drawable.images,
     };
 
     @Override
