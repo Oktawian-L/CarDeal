e2f6dea4c6698617b3dcf96d4019bca88caf9d20
Unknown <oktawianlasek@gmail.com>
2019-01-13 18:35:04 +0100

    biore nowe pliki

---
+++ app/src/main/java/pl/szop/andrzejshop/actions/UsunZUlubionych.java
@@ -0,0 +1,25 @@
+package pl.szop.andrzejshop.actions;
+
+import android.content.Context;
+
+import java.lang.reflect.InvocationTargetException;
+
+import pl.szop.andrzejshop.MyApplication;
+import pl.szop.andrzejshop.models.Product;
+
+public class UsunZUlubionych  implements Action{
+
+    public static final String NAME = "REMOVE_FROM_FAVORITES";
+
+
+    @Override
+    public void execute(Object object, Context context) {
+        try {
+            Long id = (Long) ((Product)object).getValue("id");
+            MyApplication.instance().getDataProvider().setFavorite(id, false);
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
