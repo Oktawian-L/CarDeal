a3206896a4a40e7fb112b6a45ddf04e096a7ba26
Unknown <oktawianlasek@gmail.com>
2019-01-14 21:13:11 +0100

    toolbar new options + naprawa bugow

--- app/src/main/java/pl/szop/andrzejshop/views/ProductsActivity.java
+++ app/src/main/java/pl/szop/andrzejshop/views/ProductsActivity.java
@@ -236,9 +236,11 @@ public class ProductsActivity extends AppCompatActivity implements ProductsListF
             case R.id.miProfile:
                 startActivity(new Intent(getApplicationContext(), FavoritesActivity.class));
                 return true;
-            case R.id.action_settings:
+            case R.id.miGrid:
                 startActivity(new Intent(getApplicationContext(), GridActivity.class));
                 return true;
+            case R.id.action_settings:
+                startActivity(new Intent(getApplicationContext(), ViewPagerActivity.class));
             default:
                 return super.onOptionsItemSelected(item);
         }
