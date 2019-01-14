11bab32c85bc35d021e61bf75c424020539c67a2
Unknown <oktawianlasek@gmail.com>
2019-01-14 20:55:02 +0100

    walka z leakiem pamieci

--- app/src/main/java/pl/szop/andrzejshop/views/ProductDetailsActivity.java
+++ app/src/main/java/pl/szop/andrzejshop/views/ProductDetailsActivity.java
@@ -19,6 +19,8 @@ public class ProductDetailsActivity extends AppCompatActivity implements ImagesF
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auto);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ImageView img23 = (ImageView) findViewById(R.id.jednozdjecie);
@@ -29,7 +31,6 @@ public class ProductDetailsActivity extends AppCompatActivity implements ImagesF
         Intent intent = new Intent(this,ViewPagerActivity.class);
         startActivity(intent);
     }
-
 
     @Override
     public void onFragmentInteraction(Uri uri) {
