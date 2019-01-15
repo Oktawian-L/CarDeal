31026b7f21ec514c6d6006be4063147385c73536
Unknown <oktawianlasek@gmail.com>
2019-01-15 19:58:57 +0100

    brakujący toolbar w ProductDetails

--- app/src/main/java/pl/szop/andrzejshop/views/ProductDetailsActivity.java
+++ app/src/main/java/pl/szop/andrzejshop/views/ProductDetailsActivity.java
@@ -5,6 +5,8 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -21,10 +23,10 @@ public class ProductDetailsActivity extends AppCompatActivity implements ImagesF
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auto);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbarautopoj);
         setSupportActionBar(toolbar);
         ImageView img23 = (ImageView) findViewById(R.id.jednozdjecie);
-        img23.setBackgroundResource(R.drawable.camaro3);
+        img23.setBackgroundResource(R.drawable.tiburon);
     }
     public void showGaleriePrzewijana(View view)
     {
@@ -35,5 +37,32 @@ public class ProductDetailsActivity extends AppCompatActivity implements ImagesF
     @Override
     public void onFragmentInteraction(Uri uri) {
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.miHome:
+                startActivity(new Intent(getApplicationContext(), CategoryActivity.class));
+                return true;
+            case R.id.miProfile:
+                startActivity(new Intent(getApplicationContext(), FavoritesActivity.class));
+                return true;
+            case R.id.miGrid:
+                startActivity(new Intent(getApplicationContext(), GridActivity.class));
+                return true;
+            case R.id.action_settings:
+                startActivity(new Intent(getApplicationContext(), ViewPagerActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
